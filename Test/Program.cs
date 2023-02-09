3b88329e26f8295c29d9c709f826ea18fa0b405f
AngelBrambila91 <malthael.91@gmail.com>
2023-02-09 08:22:28 -0600

    Added First Test Project to repo

---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿using Test;
+using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+TestClass tc = new TestClass(); // instancia instance
+WriteLine();
+ReadLine();
