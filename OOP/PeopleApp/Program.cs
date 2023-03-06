9e9036ba4900e42357b83ea77be0e3ad82c3829a
AngelBrambila91 <malthael.91@gmail.com>
2023-03-06 07:33:05 -0600

    Pree OOP Pt2

--- OOP/PeopleApp/Program.cs
+++ OOP/PeopleApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿using PeopleLibrary;
+using static System.Console;
 
 Person dante = new Person();
+dante.Name = "Dante";
+dante.SurName = "Cruz";
+Person luis = new Person("Luis");
+WriteLine($"Hello {dante.Name}");
--- OOP/PeopleLibrary/Person.cs
+++ OOP/PeopleLibrary/Person.cs
@@ -3,18 +3,37 @@ namespace PeopleLibrary
     public class Person
     {
         // Fields
-        public string Name;
+        public string? Name;
+        // private can be onlye accessed by class itself
+        // protected also as private and also inherited classes
         // Properties
-        public string SurName { get; set; } 
+        public string? SurName { get; set; }
 
         // Methods
-        public string getSurName()
+        public string? getSurName()
         {
             return SurName;
         }
         public void setSurname(string surname)
         {
             SurName = surname;
+        }
+        
+        // Constructor
+        // Must be named the same name of the file
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName)
+        {
+            Name = firstName;
+        }
+
+        public Person(string firstName, string surName)
+        {
+            
         }
     }
 }
